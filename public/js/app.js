01761a77c8fe378ca9eb4a9a02a2720caefd3652
Val <vcharry7@gmail.com>
2015-04-18 00:08:59 -0400

    First push to check in app

---
+++ public/js/app.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module("Passport", ["ngRoute"]);
+
+app.config(function($routeProvider) {
+    $routeProvider
+     .when('/home', {
+         templateUrl: 'views/home/home.html'
+     })
+    .when('/login', {
+        templateUrl: 'views/login/login.html',
+        controller: 'LoginCtrl'
+    })
+    .when('/profile', {
+        templateUrl: 'views/profile/profile.html'
+    })
+    .when('/contact', {
+        templateUrl: 'views/contact/contact.html'
+    })
+    .otherwise({
+        redirectTo: '/home'
+    })
+});
---
+++ public/views/login/login.js
@@ -0,0 +1,11 @@
+﻿app.controller("LoginCtrl", function ($scope, $http) {
+    $scope.login = function (user) {
+        console.log(user);
+
+        $http.post('/login', user)
+        .success(function (response) {
+            console.log(response);
+        });
+
+    };
+});
